2a42cdafc8e540e718c1b08a1d9f71d60c799146
kang0906 <jinkoo906@gmail.com>
2022-10-03 14:51:58 +0900

    start project

---
+++ springW1crud/src/main/java/com/example/springw1crud/SpringW1crudApplication.java
@@ -0,0 +1,13 @@
+package com.example.springw1crud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringW1crudApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringW1crudApplication.class, args);
+    }
+
+}
---
+++ springW1crud/src/test/java/com/example/springw1crud/SpringW1crudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springw1crud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringW1crudApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
