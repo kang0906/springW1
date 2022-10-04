0531d7dfc1d7dc9b263c3c00576769da55c24f06
kang0906 <jinkoo906@gmail.com>
2022-10-04 16:37:19 +0900

    Refactor ContentController, ContentService
    move business login in ContentController to ContentService

--- springW1crud/src/main/java/com/example/springw1crud/controller/ContentController.java
+++ springW1crud/src/main/java/com/example/springw1crud/controller/ContentController.java
@@ -27,14 +27,14 @@ public class ContentController {
 //        return contentRepository.save(content);
 //    }
 
-    @PostMapping("/content")    //게시글 작성
-    public Map<String, Object> createContent(@RequestBody ContentRequestDto requestDto){
-        Map<String, Object> map = new LinkedHashMap<>();
-        Content content = new Content(requestDto);
-        map.put("success",true);
-        map.put("data",contentRepository.save(content));
-        return map;
-    }
+//    @PostMapping("/content")    //게시글 작성
+//    public Map<String, Object> createContent(@RequestBody ContentRequestDto requestDto){
+//        Map<String, Object> map = new LinkedHashMap<>();
+//        Content content = new Content(requestDto);
+//        map.put("success",true);
+//        map.put("data",contentRepository.save(content));
+//        return map;
+//    }
 
 //    @PostMapping("/content")    //게시글 작성
 //    public ModelAndView createContent(@RequestBody ContentRequestDto requestDto){
@@ -44,59 +44,112 @@ public class ContentController {
 //        mv.addObject("success",true);
 //        return mv;
 //    }
+    @PostMapping("/content")    //게시글 작성 controller service 분리
+    public Map<String, Object> createContent(@RequestBody ContentRequestDto requestDto){
+        Map<String, Object> map = new LinkedHashMap<>();
+        map.put("success",true);
+        map.put("data",contentService.createContent(requestDto));
+        map.put("error",null);
+
+        return map;
+    }
+
+
 
 //    @GetMapping("/content")   //전체 게시글 조회
 //    public List<Content> getContents() {
 //        return contentRepository.findAllByOrderByModifiedAtDesc();
 //    }
 
+//    @GetMapping("/content")   //전체 게시글 조회
+//    public Map<String, Object> getContents() {
+//        Map<String, Object> map = new LinkedHashMap<>();
+//        map.put("success",true);
+//        map.put("data",contentRepository.findAllByOrderByModifiedAtDesc());
+//        map.put("error",false);
+//        return map;
+//    }
+
     @GetMapping("/content")   //전체 게시글 조회
     public Map<String, Object> getContents() {
         Map<String, Object> map = new LinkedHashMap<>();
         map.put("success",true);
-        map.put("data",contentRepository.findAllByOrderByModifiedAtDesc());
+        map.put("data",contentService.getContents());
         map.put("error",false);
         return map;
     }
+
+//    @GetMapping("/content/{id}")   //게시글 조회
+//    public Content getContent(@PathVariable Long id) {
+//        Content content = contentRepository.findById(id).get();
+//        log.info("password : {}",content.getPassword());
+//        return contentRepository.findById(id).get();
+//    }
 
     @GetMapping("/content/{id}")   //게시글 조회
     public Content getContent(@PathVariable Long id) {
         Content content = contentRepository.findById(id).get();
         log.info("password : {}",content.getPassword());
-        return contentRepository.findById(id).get();
+        return contentService.getContent(id);
     }
+
+//    @DeleteMapping("/content/{id}") //게시글 삭제
+//    public Long deleteMemo(@PathVariable Long id) {
+//        contentRepository.deleteById(id);
+//        return id;
+//    }
 
     @DeleteMapping("/content/{id}") //게시글 삭제
     public Long deleteMemo(@PathVariable Long id) {
-        contentRepository.deleteById(id);
+        contentService.deleteMemo(id);
         return id;
     }
 
+//    @PutMapping("/content/{id}")    //게시글 수정
+//    public Map<String, Object> updateMemo(@PathVariable Long id, @RequestBody ContentChangeDto changeDto) {
+//        Content content = contentRepository.findById(id).get();
+//        Map<String, Object> map = new LinkedHashMap<>();
+//
+//        if(content.getPassword().equals(changeDto.getRequestPassword())){
+//            ContentRequestDto requestDto = new ContentRequestDto(changeDto);
+//            contentService.update(id, requestDto);
+//            map.put("success", true);
+//            map.put("data", contentRepository.findById(id).get());
+//            map.put("error",null);
+//        }else{
+//            map.put("success", false);
+//            map.put("data", contentRepository.findById(id).get());
+//            map.put("error","비밀번호가 다릅니다.");
+//        }
+//        return map;
+//    }
+
     @PutMapping("/content/{id}")    //게시글 수정
     public Map<String, Object> updateMemo(@PathVariable Long id, @RequestBody ContentChangeDto changeDto) {
-        Content content = contentRepository.findById(id).get();
-        Map<String, Object> map = new LinkedHashMap<>();
-
-        if(content.getPassword().equals(changeDto.getRequestPassword())){
-            ContentRequestDto requestDto = new ContentRequestDto(changeDto);
-            contentService.update(id, requestDto);
-            map.put("success", true);
-            map.put("data", contentRepository.findById(id).get());
-            map.put("error",null);
-        }else{
-            map.put("success", false);
-            map.put("data", contentRepository.findById(id).get());
-            map.put("error","비밀번호가 다릅니다.");
-        }
-        return map;
+        return contentService.updateMemo(id, changeDto);
     }
 
-    @PostMapping("/password")
+//    @PostMapping("/password")   // 비밀번호 확인
+//    public Map<String, Object> checkPassword(@RequestBody CheckPasswordDto passwordDto){
+//        Content content = contentRepository.findById(passwordDto.getId()).get();
+//        Map<String, Object> map = new LinkedHashMap<>();
+//        log.info("save : {} / input : {}",content.getPassword(),passwordDto.getPassword());
+//        if(content.getPassword().equals(passwordDto.getPassword())){
+//            map.put("success",true);
+//            map.put("data",true);
+//            map.put("error",null);
+//            return map;
+//        }else{
+//            map.put("success",true);
+//            map.put("data",false);
+//            map.put("error",null);
+//            return map;
+//        }
+//    }
+    @PostMapping("/password")   // 비밀번호 확인
     public Map<String, Object> checkPassword(@RequestBody CheckPasswordDto passwordDto){
-        Content content = contentRepository.findById(passwordDto.getId()).get();
         Map<String, Object> map = new LinkedHashMap<>();
-        log.info("save : {} / input : {}",content.getPassword(),passwordDto.getPassword());
-        if(content.getPassword().equals(passwordDto.getPassword())){
+        if(contentService.checkPassword(passwordDto)){
             map.put("success",true);
             map.put("data",true);
             map.put("error",null);
--- springW1crud/src/main/java/com/example/springw1crud/service/ContentService.java
+++ springW1crud/src/main/java/com/example/springw1crud/service/ContentService.java
@@ -1,12 +1,19 @@
 package com.example.springw1crud.service;
 
+import com.example.springw1crud.dto.CheckPasswordDto;
+import com.example.springw1crud.dto.ContentChangeDto;
 import com.example.springw1crud.dto.ContentRequestDto;
 import com.example.springw1crud.entity.Content;
 import com.example.springw1crud.repository.ContentRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 
 import javax.transaction.Transactional;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
 
 @RequiredArgsConstructor
 @Service
@@ -20,5 +27,52 @@ public class ContentService {
         );
         content.update(requestDto);
         return content.getId();
+    }
+
+    @Transactional
+    public Content createContent(ContentRequestDto requestDto){ // 게시글 작성
+        Content content = new Content(requestDto);
+        return contentRepository.save(content);
+    }
+
+    public List<Content> getContents() {    // 전체 게시글 조회
+        return contentRepository.findAllByOrderByModifiedAtDesc();
+    }
+
+    @Transactional
+    public Content getContent(Long id) {
+        return contentRepository.findById(id).get();
+    }
+
+    @Transactional
+    public Long deleteMemo(Long id) {   // 게시글 삭제
+        contentRepository.deleteById(id);
+        return id;
+    }
+
+
+    @Transactional
+    public Map<String, Object> updateMemo(Long id, ContentChangeDto changeDto) {
+        Content content = contentRepository.findById(id).get();
+        Map<String, Object> map = new LinkedHashMap<>();
+
+        if(content.getPassword().equals(changeDto.getRequestPassword())){
+            ContentRequestDto requestDto = new ContentRequestDto(changeDto);
+            this.update(id, requestDto);
+            map.put("success", true);
+            map.put("data", contentRepository.findById(id).get());
+            map.put("error",null);
+        }else{
+            map.put("success", false);
+            map.put("data", contentRepository.findById(id).get());
+            map.put("error","비밀번호가 다릅니다.");
+        }
+        return map;
+    }
+
+    public boolean checkPassword(CheckPasswordDto passwordDto){
+        Content content = contentRepository.findById(passwordDto.getId()).get();
+//        log.info("save : {} / input : {}",content.getPassword(),passwordDto.getPassword());
+        return content.getPassword().equals(passwordDto.getPassword());
     }
 }
